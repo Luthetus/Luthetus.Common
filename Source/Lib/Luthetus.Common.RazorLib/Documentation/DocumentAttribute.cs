628326d1df78239029ed76e1e9f775077c84c64e
Hunter Freeman <luthetus@gmail.com>
2023-05-23 01:37:45 -0400

    ReplPage.razor

---
+++ Source/Lib/Luthetus.Common.RazorLib/Documentation/DocumentAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace Luthetus.Common.RazorLib.Documentation;
+
+public class LuthetusDocumentAttribute : Attribute
+{
+    public LuthetusDocumentAttribute(string displayName)
+    {
+        DisplayName = displayName;
+    }
+
+    public string DisplayName { get; }
+}
