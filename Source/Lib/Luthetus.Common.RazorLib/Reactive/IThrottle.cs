4c463b00254a472420251ea81c2e4dde5dabd39e
Hunter Freeman <luthetus@gmail.com>
2023-06-11 13:38:09 -0400

    OnKeyDown event causes: _mouseStoppedEventMostRecentResult = null;

--- Source/Lib/Luthetus.Common.RazorLib/Reactive/IThrottle.cs
+++ Source/Lib/Luthetus.Common.RazorLib/Reactive/IThrottle.cs
@@ -2,7 +2,7 @@
 
 public interface IThrottle : IDisposable
 {
-    public static readonly TimeSpan DefaultThrottleTimeSpan = TimeSpan.FromMilliseconds(33);
+    public static readonly TimeSpan DefaultThrottleTimeSpan = TimeSpan.FromMilliseconds(20);
 
     /// <summary>
     /// The cancellation logic should be made internal to the workItem Func itself.
