6ba8f7e290b81b45afd991db7fc7d2390a2cc604
Hunter Freeman <luthetus@gmail.com>
2023-06-11 13:48:21 -0400

    Photino app feels much better

--- Source/Lib/Luthetus.Common.RazorLib/Reactive/IThrottle.cs
+++ Source/Lib/Luthetus.Common.RazorLib/Reactive/IThrottle.cs
@@ -2,7 +2,7 @@
 
 public interface IThrottle : IDisposable
 {
-    public static readonly TimeSpan DefaultThrottleTimeSpan = TimeSpan.FromMilliseconds(20);
+    public static readonly TimeSpan DefaultThrottleTimeSpan = TimeSpan.FromMilliseconds(15);
 
     /// <summary>
     /// The cancellation logic should be made internal to the workItem Func itself.
